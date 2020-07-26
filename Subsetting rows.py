5775b4b0541218aca170e8d856662e3aa249e2d6
Abhinav Priyadarshi <rajanpan97@gmail.com>
2020-07-26 15:44:39 +0100

    Update Subsetting rows.py

--- Subsetting rows.py
+++ Subsetting rows.py
@@ -37,3 +37,10 @@ mountain_reg = homelessness[homelessness["region"] == "Mountain"]
 
 # See the result
 print(mountain_reg)
+
+# Filter for rows where family_members is less than 1000 
+# and region is Pacific
+fam_lt_1k_pac = homelessness[(homelessness["family_members"]< 1000) & (homelessness["region"]== "Pacific")]
+
+# See the result
+print(fam_lt_1k_pac)
