fa13600879cf6aa425002843c3fac299e536f688
Abhinav Priyadarshi <rajanpan97@gmail.com>
2020-07-26 03:22:35 +0100

    Create Parts of a DataFrame.py

---
+++ Parts of a DataFrame.py
@@ -0,0 +1,21 @@
+'''
+Parts of a DataFrame
+To better understand DataFrame objects, it's useful to know that they consist of three components, stored as attributes:
+
+.values: A two-dimensional NumPy array of values.
+.columns: An index of columns: the column names.
+.index: An index for the rows: either row numbers or row names.
+You can usually think of indexes as being like a list of strings or numbers, though the pandas Index data type allows for more sophisticated options. (These will be covered later in the course.)
+
+homelessness is available.
+
+Instructions
+100 XP
+Instructions
+100 XP
+Import pandas using the alias pd.
+Print a 2D NumPy array of the values in homelessness.
+Print the column names of homelessness.
+Print the index of homelessness.
+Take Hint (-30 XP)
+'''
