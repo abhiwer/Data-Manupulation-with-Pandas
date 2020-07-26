f42a3827187e66f99b057343b5d3a97883da6252
Abhinav Priyadarshi <rajanpan97@gmail.com>
2020-07-26 03:21:12 +0100

    Update Inspecting a DataFrame.py

--- Inspecting a DataFrame.py
+++ Inspecting a DataFrame.py
@@ -22,3 +22,15 @@ Take Hint (-7 XP)
 '''
 # Print the head of the homelessness data
 print(homelessness.head())
+
+#Print information about the column types and missing values in homelessness.
+# Print information about homelessness
+print(homelessness.info())
+
+#Print the number of rows and columns in homelessness.
+# Print the shape of homelessness
+print(homelessness.shape)
+
+#Print some summary statistics that describe the homelessness DataFrame.
+# Print a description of homelessness
+print(homelessness.describe())
