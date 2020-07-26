10ab7229a9a9e496d06d2d64f71d00b20459998d
Abhinav Priyadarshi <rajanpan97@gmail.com>
2020-07-26 15:42:26 +0100

    Update Subsetting rows.py

--- Subsetting rows.py
+++ Subsetting rows.py
@@ -31,3 +31,9 @@ ind_gt_10k = homelessness[homelessness["individuals"] > 10000]
 
 # See the result
 print(ind_gt_10k)
+
+# Filter for rows where region is Mountain
+mountain_reg = homelessness[homelessness["region"] == "Mountain"]
+
+# See the result
+print(mountain_reg)
