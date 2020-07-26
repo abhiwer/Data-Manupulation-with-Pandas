cb56ac4eb953afe1dcafab144f778d3558e80925
Abhinav Priyadarshi <rajanpan97@gmail.com>
2020-07-26 15:28:17 +0100

    Update Subsetting columns.py

--- Subsetting columns.py
+++ Subsetting columns.py
@@ -30,3 +30,9 @@ individuals = homelessness["individuals"]
 
 # Print the head of the result
 print(individuals.head())
+
+# Select the state and family_members columns
+state_fam = homelessness[["state", "family_members"]]
+
+# Print the head of the result
+print(state_fam.head())
