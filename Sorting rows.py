5aeda9b82bb44c06d8b941dafcf1466e25c2ace1
Abhinav Priyadarshi <rajanpan97@gmail.com>
2020-07-26 03:37:45 +0100

    Update Sorting rows.py

--- Sorting rows.py
+++ Sorting rows.py
@@ -32,3 +32,9 @@ homelessness_ind = homelessness.sort_values("individuals")
 
 # Print the top few rows
 print(homelessness_ind.head())
+
+# Sort homelessness by descending family members
+homelessness_fam = homelessness.sort_values("family_members", ascending= False)
+
+# Print the top few rows
+print(homelessness_fam.head())
